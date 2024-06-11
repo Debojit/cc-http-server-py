63fc0d27b74b89ffcc89766621624822ee99e75a
Debojit Sinha <sinha.debojit@gmail.com>
2024-06-12 00:26:28 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 def parse_request(http_msg:str) -> dict:
     http_req:list = http_msg.split(CRLF*2)
@@ -19,41 +20,43 @@ def parse_request(http_msg:str) -> dict:
     }
     return parsed_msg
 
-def main() -> None:
-    print("Logs from your program will appear here!")
+def handle_request(client:socket.socket) -> None:
+    with client:
+        req_msg:str = ""
+        end_of_msg:bool = False
+        while not end_of_msg:
+            req_buf:str = client.recv(1024).decode("utf8")
+            if len(req_buf) > 0:
+                end_of_msg = True
+            req_msg += req_buf
+            
+        print(f"Request Message: {req_msg}")
 
+        http_req:dict = parse_request(req_msg)
+        
+        url:str = http_req["url"]
+        match url:
+            case _ if url == "/":
+                client.send("HTTP/1.1 200 OK\r\n\r\n".encode("utf8"))
+            case _ if url.startswith("/echo"):
+                echo_msg:str = http_req["url"].split("/")[2]
+                client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(echo_msg)}{CRLF}{CRLF}{echo_msg}".encode("utf8"))
+            case _ if url == "/user-agent":
+                usr_agent:str = http_req["headers"]["user-agent"]
+                client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(usr_agent)}{CRLF}{CRLF}{usr_agent}".encode("utf8"))
+            case _:
+                client.send(f"HTTP/1.1 404 Not Found{CRLF}{CRLF}".encode("utf8"))
+
+def main() -> None:
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True) # Open the socket
     server_socket.listen() # Start listening
     
+    threads:list = []
     while True:
         client, _ = server_socket.accept() # Accept incoming connections
-
-        with client:
-            req_msg:str = ""
-            end_of_msg:bool = False
-            while not end_of_msg:
-                req_buf:str = client.recv(1024).decode("utf8")
-                if len(req_buf) > 0:
-                    end_of_msg = True
-                req_msg += req_buf
-                
-            print(f"Request Message: {req_msg}")
-
-            http_req:dict = parse_request(req_msg)
-            
-            url:str = http_req["url"]
-            match url:
-                case _ if url == "/":
-                    client.send("HTTP/1.1 200 OK\r\n\r\n".encode("utf8"))
-                case _ if url.startswith("/echo"):
-                    echo_msg:str = http_req["url"].split("/")[2]
-                    client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(echo_msg)}{CRLF}{CRLF}{echo_msg}".encode("utf8"))
-                case _ if url == "/user-agent":
-                    usr_agent:str = http_req["headers"]["user-agent"]
-                    client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(usr_agent)}{CRLF}{CRLF}{usr_agent}".encode("utf8"))
-                case _:
-                    client.send(f"HTTP/1.1 404 Not Found{CRLF}{CRLF}".encode("utf8"))
-
+        thread:Thread = Thread(target=handle_request, args=[client])
+        threads.append(thread)
+        thread.run()
 
 if __name__ == "__main__":
     CRLF:str = "\r\n"
