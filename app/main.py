da8e1ec452fe399a099f59762234777a49940756
Debojit Sinha <sinha.debojit@gmail.com>
2024-06-12 00:16:27 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,24 @@
 import socket
 
+def parse_request(http_msg:str) -> dict:
+    http_req:list = http_msg.split(CRLF*2)
+    
+    # Parse HTTP request line
+    req_line:str = http_req[0].split(CRLF)[0]
+    split_req_line:list = req_line.split(" ")
+
+    # Get HTTP headers
+    http_headers:list = http_req[0].split(CRLF)[1:]
+
+    parsed_msg = {
+        "http_version": split_req_line[2],
+        "verb": split_req_line[0],
+        "url": split_req_line[1],
+        "headers" : {header.split(":")[0].lower().strip() : header.split(":")[1].strip() for header in http_headers},
+        "body": http_req[1]
+    }
+    return parsed_msg
+
 def main() -> None:
     print("Logs from your program will appear here!")
 
@@ -9,31 +28,31 @@ def main() -> None:
     while True:
         client, _ = server_socket.accept() # Accept incoming connections
 
-        req_msg:str = ""
-        end_of_msg:bool = False
-        while not end_of_msg:
-            req_buf:str = client.recv(1024).decode("utf8")
-            if len(req_buf) > 0:
-                end_of_msg = True
-            req_msg += req_buf
+        with client:
+            req_msg:str = ""
+            end_of_msg:bool = False
+            while not end_of_msg:
+                req_buf:str = client.recv(1024).decode("utf8")
+                if len(req_buf) > 0:
+                    end_of_msg = True
+                req_msg += req_buf
+                
+            print(f"Request Message: {req_msg}")
+
+            http_req:dict = parse_request(req_msg)
             
-        print(f"Request Message: {req_msg}")
-
-        http_req:list = req_msg.split(CRLF)
-        http_req_line:str = http_req[0]
-        url:str = http_req_line.split(" ")[1]
-
-        if url == "/":
-            pass
-
-        match url.split("/")[1]:
-            case "":
-                client.send("HTTP/1.1 200 OK\r\n\r\n".encode("utf8"))
-            case "echo":
-                echo_msg:str = url.split("/")[2]
-                client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(echo_msg)}{CRLF}{CRLF}{echo_msg}".encode("utf8"))
-            case _:
-                client.send(f"HTTP/1.1 404 Not Found{CRLF}{CRLF}".encode("utf8"))
+            url:str = http_req["url"]
+            match url:
+                case _ if url == "/":
+                    client.send("HTTP/1.1 200 OK\r\n\r\n".encode("utf8"))
+                case _ if url.startswith("/echo"):
+                    echo_msg:str = http_req["url"].split("/")[2]
+                    client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(echo_msg)}{CRLF}{CRLF}{echo_msg}".encode("utf8"))
+                case _ if url == "/user-agent":
+                    usr_agent:str = http_req["headers"]["user-agent"]
+                    client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(usr_agent)}{CRLF}{CRLF}{usr_agent}".encode("utf8"))
+                case _:
+                    client.send(f"HTTP/1.1 404 Not Found{CRLF}{CRLF}".encode("utf8"))
 
 
 if __name__ == "__main__":
