3994eb8d64aefceb13f1cf229913c3f5f7da0358
Debojit Sinha <sinha.debojit@gmail.com>
2024-06-11 17:15:00 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,9 +8,11 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True) # Open the socket
+    server_socket.listen() # Start listening
 
-
+    while True:
+        client, address = server_socket.accept() # Accept incoming connections
+        client.send("HTTP/1.1 200 OK\r\n\r\n".encode("utf8"))
 if __name__ == "__main__":
     main()
