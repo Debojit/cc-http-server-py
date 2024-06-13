7519c6a314ba0427bd30bb45453d1585546cab95
Debojit Sinha <sinha.debojit@gmail.com>
2024-06-13 10:26:12 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
+import sys
 import socket
 from threading import Thread
 from pathlib import Path
+
 
 def parse_request(http_msg:str) -> dict:
     http_req:list = http_msg.split(CRLF*2)
@@ -21,6 +23,43 @@ def parse_request(http_msg:str) -> dict:
     }
     return parsed_msg
 
+def handle_get(client:socket, http_req:dict) -> None:
+    url:str = http_req["url"]
+    match url:
+            case _ if url == "/":
+                client.send(f"HTTP/1.1 200 OK{CRLF}{CRLF}".encode("utf8"))
+            case _ if url.startswith("/echo"):
+                echo_msg:str = http_req["url"].split("/")[2]
+                client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(echo_msg)}{CRLF}{CRLF}{echo_msg}".encode("utf8"))
+            case _ if url == "/user-agent":
+                usr_agent:str = http_req["headers"]["user-agent"]
+                client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(usr_agent)}{CRLF}{CRLF}{usr_agent}".encode("utf8"))
+            case _ if url.startswith("/files"):
+                file_name:str = url.split("/files/")[-1]
+                print(f"Reading file {sys.argv[2]}/{file_name}")
+                return_file:Path = Path(f"{sys.argv[2]}/{file_name}")
+                if return_file.exists():
+                    with open(return_file, mode="r") as output_file:
+                        file_data:str = "".join(output_file.readlines())
+                    client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: application/octet-stream{CRLF}Content-Length: {len(file_data)}{CRLF}{CRLF}{file_data}".encode("utf8"))
+                else:
+                    client.send(f"HTTP/1.1 404 Not Found{CRLF}{CRLF}".encode("utf8"))
+            case _:
+                client.send(f"HTTP/1.1 404 Not Found{CRLF}{CRLF}".encode("utf8"))
+
+def handle_post(client:socket, http_req:dict) -> None:
+    url:str = http_req["url"]
+    match url:
+        case _ if url.startswith("/files"):
+            file_name:str = url.split("/files/")[-1]
+            data:str = http_req["body"]
+            with open(f"{sys.argv[2]}/{file_name}", "w") as data_file:
+                print(f"Writing file {sys.argv[2]}{file_name}")
+                data_file.write(data)
+            client.send(f"HTTP/1.1 201 Created{CRLF}{CRLF}".encode("utf8"))
+        case _:
+            client.send(f"HTTP/1.1 404 Not Found{CRLF}{CRLF}".encode("utf8"))
+
 def handle_request(client:socket.socket) -> None:
     with client:
         req_msg:str = ""
@@ -35,27 +74,14 @@ def handle_request(client:socket.socket) -> None:
 
         http_req:dict = parse_request(req_msg)
         
-        url:str = http_req["url"]
-        match url:
-            case _ if url == "/":
-                client.send("HTTP/1.1 200 OK\r\n\r\n".encode("utf8"))
-            case _ if url.startswith("/echo"):
-                echo_msg:str = http_req["url"].split("/")[2]
-                client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(echo_msg)}{CRLF}{CRLF}{echo_msg}".encode("utf8"))
-            case _ if url == "/user-agent":
-                usr_agent:str = http_req["headers"]["user-agent"]
-                client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: text/plain{CRLF}Content-Length: {len(usr_agent)}{CRLF}{CRLF}{usr_agent}".encode("utf8"))
-            case _ if url.startswith("/files"):
-                file_name:str = url.split("/files")[-1]
-                return_file:Path = Path(f"/tmp/data/codecrafters.io/http-server-tester/{file_name}")
-                if return_file.exists():
-                    with open(return_file, mode="r") as output_file:
-                        file_data:str = "".join(output_file.readlines())
-                    client.send(f"HTTP/1.1 200 OK{CRLF}Content-Type: application/octet-stream{CRLF}Content-Length: {len(file_data)}{CRLF}{CRLF}{file_data}".encode("utf8"))
-                else:
-                    client.send(f"HTTP/1.1 404 Not Found{CRLF}{CRLF}".encode("utf8"))
+        verb:str = http_req["verb"]
+        match verb:
+            case "GET":
+                handle_get(client, http_req)
+            case "POST":
+                handle_post(client, http_req)
             case _:
-                client.send(f"HTTP/1.1 404 Not Found{CRLF}{CRLF}".encode("utf8"))
+                client.send(f"HTTP/1.1 405 Method Not Allowed{CRLF}{CRLF}".encode("utf8"))
 
 def main() -> None:
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True) # Open the socket
